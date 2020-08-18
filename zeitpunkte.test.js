4fe1c0c4b186365840aa43b148deae1d4a409dad
David <learnitmyway@users.noreply.github.com>
2020-08-18 11:16:36 +0200

    one day, 5 minutes, phase start before day start, phase end after day end, Iceland

--- zeitpunkte.test.js
+++ zeitpunkte.test.js
@@ -66,3 +66,20 @@ test('one day, 5 minutes, phase start before day start, phase end after day end,
   expect(actual[0]).toEqual(new Date('Tue Aug 03 2020 07:00 GMT+0000'))
   expect(actual[5]).toEqual(new Date('Tue Aug 03 2020 07:05 GMT+0000'))
 })
+
+test('one day, 5 minutes, phase start before day start, phase end after day end, Iceland', () => {
+  const input = {
+    day_end: '08:05',
+    day_start: '08:00',
+    phase_end: '2020-08-03T09:05',
+    phase_start: '2020-08-03T08:00',
+    week: '0010000',
+    time_zone: 'Iceland',
+  }
+
+  const actual = zeitpunkte(input)
+
+  expect(actual.length).toBe(6)
+  expect(actual[0]).toEqual(new Date('Tue Aug 03 2020 08:00 GMT+0000'))
+  expect(actual[5]).toEqual(new Date('Tue Aug 03 2020 08:05 GMT+0000'))
+})
