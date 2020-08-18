20fb79ee0f71335a0165f2a40657dea0a3e05e31
David <learnitmyway@users.noreply.github.com>
2020-08-18 11:06:47 +0200

    one day, 5 minutes, phase start before day start, Europe/Berlin

--- zeitpunkte.js
+++ zeitpunkte.js
@@ -1,17 +1,37 @@
 const moment = require('moment-timezone')
 
+const millisInOneMinute = 1000 * 60
+
 export default function zeitpunkte(input) {
   const [dayStartHours, dayStartMinutes] = input.day_start.split(':')
   const [dayEndHours, dayEndMinutes] = input.day_end.split(':')
-  const diffMinutes =
-    (dayEndHours - dayEndMinutes) * 60 + (dayEndMinutes - dayStartMinutes)
 
-  const phaseStartMillis = moment(input.phase_start).valueOf()
+  const phaseStart = moment(input.phase_start)
+  const phaseStartMillis = phaseStart.valueOf()
+  const phaseStartMinutes = phaseStartMillis / millisInOneMinute
+  const phaseEndMillis = moment(input.phase_end).valueOf()
+  const diffPhaseMillis = phaseEndMillis - phaseStartMillis
 
-  const arr = [phaseStartMillis]
+  const diffPhaseMinutes = diffPhaseMillis / millisInOneMinute
 
-  for (let i = 1; i <= diffMinutes; i++) {
-    arr[i] = arr[i - 1] + 1000 * 60
+  const phaseStartDayStart = phaseStart
+    .hours(dayStartHours)
+    .minutes(dayStartMinutes)
+  const phaseStartDayStartMillis = phaseStartDayStart.valueOf()
+  const phaseStartDayStartMinutes = phaseStartDayStartMillis / millisInOneMinute
+
+  const arr = []
+
+  for (let i = 0; i <= diffPhaseMinutes; i++) {
+    const currentMinute = i + phaseStartMinutes
+
+    if (currentMinute >= phaseStartDayStartMinutes) {
+      if (arr.length === 0) {
+        arr.push(currentMinute * millisInOneMinute)
+      } else {
+        arr.push(arr[arr.length - 1] + millisInOneMinute)
+      }
+    }
   }
 
   return arr.map((val) => new Date(val))
--- zeitpunkte.test.js
+++ zeitpunkte.test.js
@@ -10,11 +10,25 @@ test('one day, 5 minutes, same phase as day, Europe/Berlin', () => {
     time_zone: 'Europe/Berlin',
   }
 
-  expect(zeitpunkte(input)[0]).toEqual(
-    new Date('Tue Aug 03 2020 06:00 GMT+0000')
-  )
+  const actual = zeitpunkte(input)
 
-  expect(zeitpunkte(input)[5]).toEqual(
-    new Date('Tue Aug 03 2020 06:05 GMT+0000')
-  )
+  expect(actual[0]).toEqual(new Date('Tue Aug 03 2020 06:00 GMT+0000'))
+  expect(actual[5]).toEqual(new Date('Tue Aug 03 2020 06:05 GMT+0000'))
+})
+
+test('one day, 5 minutes, phase start before day start, Europe/Berlin', () => {
+  const input = {
+    day_end: '08:05',
+    day_start: '08:00',
+    phase_end: '2020-08-03T08:05',
+    phase_start: '2020-08-03T07:00',
+    week: '0010000',
+    time_zone: 'Europe/Berlin',
+  }
+
+  const actual = zeitpunkte(input)
+
+  expect(actual.length).toBe(6)
+  expect(actual[0]).toEqual(new Date('Tue Aug 03 2020 06:00 GMT+0000'))
+  expect(actual[5]).toEqual(new Date('Tue Aug 03 2020 06:05 GMT+0000'))
 })
