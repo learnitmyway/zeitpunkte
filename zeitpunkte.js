5b860492cafb0361229802184c93f2065b19697f
David <learnitmyway@users.noreply.github.com>
2020-08-18 10:33:46 +0200

    one day, 5 minutes, same phase as day, Europe/Berlin

--- zeitpunkte.js
+++ zeitpunkte.js
@@ -1,3 +1,18 @@
-export default function zeitpunkte() {
-  return false
+const moment = require('moment-timezone')
+
+export default function zeitpunkte(input) {
+  const [dayStartHours, dayStartMinutes] = input.day_start.split(':')
+  const [dayEndHours, dayEndMinutes] = input.day_end.split(':')
+  const diffMinutes =
+    (dayEndHours - dayEndMinutes) * 60 + (dayEndMinutes - dayStartMinutes)
+
+  const phaseStartMillis = moment(input.phase_start).valueOf()
+
+  const arr = [phaseStartMillis]
+
+  for (let i = 1; i <= diffMinutes; i++) {
+    arr[i] = arr[i - 1] + 1000 * 60
+  }
+
+  return arr.map((val) => new Date(val))
 }
--- zeitpunkte.test.js
+++ zeitpunkte.test.js
@@ -1,5 +1,20 @@
 import zeitpunkte from './zeitpunkte'
 
-test('test', () => {
-  expect(zeitpunkte()).toBe(true)
+test('one day, 5 minutes, same phase as day, Europe/Berlin', () => {
+  const input = {
+    day_end: '08:05',
+    day_start: '08:00',
+    phase_end: '2020-08-03T08:05',
+    phase_start: '2020-08-03T08:00',
+    week: '0010000',
+    time_zone: 'Europe/Berlin',
+  }
+
+  expect(zeitpunkte(input)[0]).toEqual(
+    new Date('Tue Aug 03 2020 06:00 GMT+0000')
+  )
+
+  expect(zeitpunkte(input)[5]).toEqual(
+    new Date('Tue Aug 03 2020 06:05 GMT+0000')
+  )
 })
